5067ddf7522e39c403ceccc817e6a86fe66bb944
kinlaten <kincongviec@gmail.com>
2025-10-19 17:35:59 +1100

    Config connection to local postgres, init migration  for db

---
+++ Migrations/20251019063217_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    ProductUrl = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    RetailerName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Brand = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    ImageUrl = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Price",
+                table: "Product",
+                column: "Price");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_RetailerName",
+                table: "Product",
+                column: "RetailerName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ZeniSearch.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Register DBContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+//Add Controlers and built-in OpenApi
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    //add scalar ui
+
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers(); //use controller instead of minimal api
 
 app.Run();
